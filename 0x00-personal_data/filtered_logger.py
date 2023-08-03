aff939b7c8a15c2b145b4095a257f2872fdcbde4
samsonkimani <samsonkimani43@gmail.com>
2023-08-03 17:15:17 +0300

    updating the filtered file to have a get logger method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 import re
 import logging
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password',)
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(
