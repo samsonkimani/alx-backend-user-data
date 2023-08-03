1fa5942e2a9939b8c3932834a716b5e2622248fb
samsonkimani <samsonkimani43@gmail.com>
2023-08-03 14:16:27 +0300

    creating the filter function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,11 +3,13 @@
 """
 filter_datum module
 """
-
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str],
+        redaction: str, message: str, separator: str) -> str:
     """ a function to filter out set values and add reductions"""
     for field in fields:
         message = re.sub(field + '=.*?' + separator,
