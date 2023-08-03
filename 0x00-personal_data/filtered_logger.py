53a2dc146f988860806a54806d8603c517f8966b
samsonkimani <samsonkimani43@gmail.com>
2023-08-03 16:20:44 +0300

    creating the logger class and the logger formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ filter_datum module
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(
@@ -15,3 +16,25 @@ def filter_datum(
         message = re.sub(field + '=.*?' + separator,
                          field + '=' + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(
+            self.fields,
+            self.REDACTION,
+            message,
+            self.SEPARATOR)
+        return redacted
