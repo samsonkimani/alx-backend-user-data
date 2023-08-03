6ef644ce88704a4db3fe2fd18a8514bab212a96a
samsonkimani <samsonkimani43@gmail.com>
2023-08-03 19:36:10 +0300

    checking the validity of a password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,7 +2,7 @@
 """encrypt a password
 """
 import bcrypt
-from bcrypt import hashpw
+from bcrypt import hashpw, checkpw
 
 
 def hash_password(password: str) -> bytes:
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     passwd = password.encode()
     hashed = hashpw(passwd, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ a function to check the validity of a password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
