f77c65b3efa0dcf278b6528489aba82b6e0d13d8
samsonkimani <samsonkimani43@gmail.com>
2023-08-07 18:03:27 +0300

    need authorization

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -9,7 +9,24 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require auth function that returns false"""
-        return False
+        if excluded_paths and path:
+            if path[-1] == '/':
+                new_path = path[:-1]
+            else:
+                new_path = path
+            new_excluded_path = []
+            for element in excluded_paths:
+                if element[-1] == '/':
+                    new_excluded_path.append(element[:-1])
+
+            if new_path not in new_excluded_path:
+                return True
+            else:
+                return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header"""
