ee9a3c8069e61dd4f696997040e5e48cf86b0851
samsonkimani <samsonkimani43@gmail.com>
2023-08-08 12:22:12 +0300

    doctyping the before function

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -39,21 +39,21 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
-@app.before_request
-def before_request() -> str:
-    """ handle before request"""
-    if auth is not None:
-        return
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+# @app.before_request
+# def before_request() -> str:
+#     """ handle before request"""
+#     if auth is not None:
+#         return
+#     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
-    app.logger.info("path")
+#     app.logger.info("path")
 
-    if auth.require_path(request.path, paths) is True:
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+#     if auth.require_path(request.path, paths) is True:
+#         return
+#     if auth.authorization_header(request) is None:
+#         abort(401)
+#     if auth.current_user(request) is None:
+#         abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
