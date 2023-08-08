2de51f1f8fddf3640cc6dc0cfba7dda63d3d62ec
samsonkimani <samsonkimani43@gmail.com>
2023-08-08 16:19:23 +0300

    decoding base64 to string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,10 +4,12 @@ creating the basic auth module
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ for basic authentication"""
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """ extracting the basic key"""
@@ -20,3 +22,17 @@ class BasicAuth(Auth):
             return None
         else:
             return header_array[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ decode base64 header"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_string = base64.b64decode(base64_authorization_header)
+            final_string = decoded_string.decode('utf-8')
+            return final_string
+        except BaseException:
+            return None
