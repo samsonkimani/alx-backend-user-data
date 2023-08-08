d9b31339584330a5cc0a8bb866afeb64da9efc7e
samsonkimani <samsonkimani43@gmail.com>
2023-08-08 22:47:41 +0300

    updating basic auth file to return user

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,10 +14,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv("AUTH_TYPE")
 
-if auth_type != 'basic_auth':
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-else:
+elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,13 +65,13 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         try:
-            users = Users.search({"email": user_email})
+            users = Users.search(email=user_email)
             if not users or users == []:
                 return None
-            for user in users:
-                if user.is_valid_password(user_pwd):
-                    return user
-            return None
+            user = users[0]
+            if not user.is_valid_password(user_pwd):
+                return None
+            return user
         except Exception:
             return None
 
