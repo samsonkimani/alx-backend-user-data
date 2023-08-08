f72bd66127f690cdcaff004f604be4fedebfa9e2
samsonkimani <samsonkimani43@gmail.com>
2023-08-08 14:08:37 +0300

    base64 authorizarion header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ for basic authentication"""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ extracting the basic key"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        header_array = authorization_header.split(" ")
+        if header_array[0] != "Basic":
+            return None
+        else:
+            return header_array[1]
