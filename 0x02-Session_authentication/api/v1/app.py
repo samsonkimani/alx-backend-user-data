969879d051b391c7fe0285a292918c5e1536218a
samsonkimani <samsonkimani43@gmail.com>
2023-08-10 13:14:15 +0300

    updating the before request to cater for session cookies

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,10 +32,12 @@ def before_request() -> str:
         pass
     else:
         paths = ['/api/v1/status/',
-                 '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                 '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                 '/api/v1/auth_session/login/']
 
         if auth.require_auth(request.path, paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
                 abort(401, description="Unauthorized")
             request.current_user = auth.current_user(request)
             if request.current_user is None:
