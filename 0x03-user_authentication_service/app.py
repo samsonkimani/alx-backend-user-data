cf9193446d30ffa0168da6f1b697be177dd40ea4
samsonkimani <samsonkimani43@gmail.com>
2023-08-16 18:45:39 +0300

    updating the user password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 """ creating a flask app"""
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
 
 AUTH = Auth()
+
+def get_reset_password_token():
+    """ a function to get the reset password token"""
+    try:
+        email = request.form.get("email")
+        user = AUTH.find_user_by(email=email)
+        if user:
+            token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": email, "reset_token": token}), 200
+        abort(403)
+    except NoResutFound:
+        return None
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
@@ -41,7 +54,29 @@ def login():
             response.set_cookie('session_id', session_id)
             return response
 
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """ logout a user"""
+    try:
+        session_id = request.cookie.get("session_id")
+        user = AUTH.get_user_from_session(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
+    except NoResultFound:
+        abort(403)
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def get_profile():
+    """ return the user based on session id"""
+    try:
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+        abort(403)
+    except NoResultFound:
+        return None
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,7 +56,7 @@ class Auth:
                 return False
         except NoResultFound:
             return False
-    
+
     def _generate_uuid(self):
         """ a function to generate uuid"""
         _uuid = uuid4()
@@ -81,3 +81,32 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id):
+        """ destroy a session"""
+        self._db.update_user(user_id, session_id=None)
+        return None
+
+    def get_reset_password_token(self, email):
+        """ generate a password reset token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                token = self._generate_uuid()
+                self._db.update_user(user.id, reset_token=token)
+                return token
+            raise ValueError("no such values were found")
+        except NoResultFound:
+            return None
+
+    def update_password(reset_token, password):
+        """ update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            if user:
+                hashed_password = _hash_password(password)
+                self._db.update_user(user.id, hashed_password=hashed_password)
+                self._db.update_user(user.id, reset_token=None)
+            else:
+                raise ValueError("value does not exist")
+        except NoResultFound:
+            return None
