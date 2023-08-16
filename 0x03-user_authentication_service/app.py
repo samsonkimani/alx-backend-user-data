d54407f76eb1f0d013cb05726f333b27ab5a4dca
samsonkimani <samsonkimani43@gmail.com>
2023-08-16 11:34:25 +0300

    creating a new user from a form

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 """ creating a flask app"""
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def home():
     """ route to home page"""
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def creating_user():
+    """ function to create a user"""
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = AUTH.register_user(email, password)
+        if user:
+            return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
