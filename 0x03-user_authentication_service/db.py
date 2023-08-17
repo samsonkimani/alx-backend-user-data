aa5b02bd1d361e872f88e9604c97013dcd835993
samsonkimani <samsonkimani43@gmail.com>
2023-08-17 12:07:52 +0300

    updating the add user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,27 +50,27 @@ class DB:
             self._session.rollback()
             raise e
 
-    def find_user_by(self, **kwargs: Mapping) -> User:
-        """ return user by filter value"""
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound("No results found")
-            return user
-        except NoResultFound:
-            raise NoResultFound("No results found")
-        except InvalidRequestError:
-            raise InvalidRequestError("not a valid request")
+    # def find_user_by(self, **kwargs: Mapping) -> User:
+    #     """ return user by filter value"""
+    #     try:
+    #         user = self._session.query(User).filter_by(**kwargs).first()
+    #         if user is None:
+    #             raise NoResultFound("No results found")
+    #         return user
+    #     except NoResultFound:
+    #         raise NoResultFound("No results found")
+    #     except InvalidRequestError:
+    #         raise InvalidRequestError("not a valid request")
 
-    def update_user(self, user_id: str, **kwargs: dict[str, str]) -> None:
-        """ a function to update the user"""
-        try:
-            user = self.find_user_by(id=user_id)
-            for k, v in kwargs.items():
-                if hasattr(user, k):
-                    setattr(user, k, v)
-                else:
-                    raise ValueError("Such attributes do not exist")
-            self._session.commit()
-        except NoResultFound:
-            raise NoResultFound("results not found")
+    # def update_user(self, user_id: str, **kwargs: dict[str, str]) -> None:
+    #     """ a function to update the user"""
+    #     try:
+    #         user = self.find_user_by(id=user_id)
+    #         for k, v in kwargs.items():
+    #             if hasattr(user, k):
+    #                 setattr(user, k, v)
+    #             else:
+    #                 raise ValueError("Such attributes do not exist")
+    #         self._session.commit()
+    #     except NoResultFound:
+    #         raise NoResultFound("results not found")
