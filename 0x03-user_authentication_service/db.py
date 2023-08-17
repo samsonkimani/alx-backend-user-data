8a7bd66c1184d5cd9774c7e0acff4b3eef1139eb
samsonkimani <samsonkimani43@gmail.com>
2023-08-17 12:43:56 +0300

    updating the db file

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,31 +1,31 @@
-"""DB module
+#!/usr/bin/env python3
+"""DB module.
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.exc import SQLAlchemyError
-from user import User
-from user import Base
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initialize a new DB instance.
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -33,18 +33,13 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ method to add a user to a database"""
-
-        new_user = User(
-            email=email,
-            hashed_password=hashed_password,
-            session_id='',
-            reset_token='')
-        self._session.add(new_user)
-
+        """Adds a new user to the database.
+        """
         try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
             self._session.commit()
-            return new_user
-        except SQLAlchemyError as e:
+        except Exception:
             self._session.rollback()
-            raise e
+            new_user = None
+        return new_user
--- 0x03-user_authentication_service/hold_file.py
+++ 0x03-user_authentication_service/hold_file.py
@@ -22,3 +22,56 @@
             self._session.commit()
         except NoResultFound:
             raise NoResultFound("results not found")
+
+
+
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.exc import SQLAlchemyError
+from user import User
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ method to add a user to a database"""
+
+        new_user = User(
+            email=email,
+            hashed_password=hashed_password,
+            session_id='',
+            reset_token='')
+        self._session.add(new_user)
+
+        try:
+            self._session.commit()
+            return new_user
+        except SQLAlchemyError as e:
+            self._session.rollback()
+            raise e
