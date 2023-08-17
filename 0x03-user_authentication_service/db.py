8f0a26511e97caa6e160ffdf841261817e9c5566
samsonkimani <samsonkimani43@gmail.com>
2023-08-17 12:51:24 +0300

    updating the password

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -55,3 +55,16 @@ class DB:
             raise NoResultFound("No results found")
         except InvalidRequestError:
             raise InvalidRequestError("not a valid request")
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ a function to update the user"""
+        try:
+            user = self.find_user_by(id=user_id)
+            for k, v in kwargs.items():
+                if hasattr(user, k):
+                    setattr(user, k, v)
+                else:
+                    raise ValueError("Such attributes do not exist")
+            self._session.commit()
+        except NoResultFound:
+            raise NoResultFound("results not found")
